bdec1b4365eea6d60b282210216eb1541c107874
Sølvberg <54006907+Snullerberg@users.noreply.github.com>
2024-06-09 22:04:48 +0200

    C

--- Languages/quicksort.c
+++ Languages/quicksort.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 
+
 void swap(int *a, int *b){
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-int Partition(int in[], int p1, int p2){
-  int x = in[p2];
-  int i = p1 - 1;
 
-  for(int j = p1; j <= p2 - 1; j++ ){
-    if(in[j] < x){
-      i++;
-      swap(&in[i], &in[j]);
+int Partition(int A[], int p1, int p2){
+    int pivot = A[p2];
+    int i = p1 - 1;
+
+    for(int j = p1; j <= p2 - 1; j++ ){
+        if(A[j] < pivot){
+        i++;
+        swap(&A[i], &A[j]);
+        }
     }
-  }
-  swap(&in[i+1], &in[p2]);
-  return(i+1);
+    swap(&A[i + 1], &A[p2]);
+    return(i + 1);
 }
 
-void QuickSort(int in[], int p1, int p2){
-  if(p1 < p2){
-    int x = Partition(in, p1, p2);
-    QuickSort(in, p1, x - 1);
-    QuickSort(in, x + 1, p2);
-  }
+
+void QuickSort(int A[], int p1, int p2){
+    if(p1 < p2){
+        int q = Partition(A, p1, p2);
+        QuickSort(A, p1, q - 1);
+        QuickSort(A, q + 1, p2);
+    }
 }
+
 
 int main(){
-  int unsorted[] = {572, -856, 483, -74, 901, -399, 207, 692, -52, -908, 317, 145, -501, 623, 0, -310, 25, -657, 448, 9};
-  int n = sizeof(unsorted)/sizeof(unsorted[0]) - 1;
+    int A[] = {572, -856, 483, -74, 901, -399, 207, 692, -52, -908, 317, 145, -501, 623, 0, -310, 25, -657, 448, 9};
+    QuickSort(unsorted, 0, sizeof(A)/sizeof(A[0]) - 1);
   
-  QuickSort(unsorted, 0, n);
+    for(int i = 0; i <= n; i++){
+        printf("%d ", A[i]);
+    }
   
-  for(int i = 0; i <= n; i++){
-    printf("%d ", unsorted[i]);
-  }
-  
-  return 0;
+    return 0;
 }
