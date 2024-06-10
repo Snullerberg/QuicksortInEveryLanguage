1db8030081086cee38a41d832df2a152f44763d5
Sølvberg <54006907+Snullerberg@users.noreply.github.com>
2024-06-10 14:34:10 +0200

    Python

--- Languages/quicksort.py
+++ Languages/quicksort.py
@@ -1,6 +1,5 @@
 A = [572, -856, 483, -74, 901, -399, 207, 692, -52, -908, 317, 145, -501, 623, 0, -310, 25, -657, 448, 9]
 
-
 def Partition(A, p1, p2):
     pivot = A[p2]
     i = p1 - 1
@@ -11,13 +10,11 @@ def Partition(A, p1, p2):
     A[i + 1], A[p2] = A[p2], A[i + 1]
     return i + 1
 
-
 def QuickSort(A, p1, p2):
     if p1 < p2:
         q = Partition(A, p1, p2)
         QuickSort(A, p1, q - 1)
         QuickSort(A, q + 1, p2)
-
 
 if __name__ == "__main__":
     QuickSort(A, 0, len(A) - 1)
